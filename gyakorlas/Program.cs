e832661dd1ac46f2e3c6db85e3cb1e2eb63554de
Freay2001 <ehreth.lili@gmail.com>
2021-02-04 20:31:44 +0100

    initial commit

---
+++ gyakorlas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ugyfelszolgalat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Adj egy számot, b+!");
+                string input = Console.ReadLine();
+                int inputSzam;
+                bool sikerultKonvertalni = int.TryParse(input, out inputSzam);
+                if (sikerultKonvertalni)
+                {
+                    if (inputSzam % 2 != 0)
+                    {
+                        Console.WriteLine("A(z) " + inputSzam + " páratlan.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("A(z) " + inputSzam + " páros.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Hülye vagy b+!");
+                }
+            }
+            // for ciklus legközelebb
+        }
+    }
+}
